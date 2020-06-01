196763c6973af1dcd94bcd255d436adbcde6eafa
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2020-06-01 16:38:28 +0100

    Updates

---
+++ precache-manifest.de0f02cbef9c5ee5bd369668f7ec16be.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5715fb59ff3c6ca07170540106f9cd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f6854d70980e5f6590",
+    "url": "/static/css/main.97519260.chunk.css"
+  },
+  {
+    "revision": "fa7cc8d57c5b7087637a",
+    "url": "/static/js/2.e58107a4.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.e58107a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f6854d70980e5f6590",
+    "url": "/static/js/main.8fac1113.chunk.js"
+  },
+  {
+    "revision": "a3ef0b287dfd086ce545",
+    "url": "/static/js/runtime-main.19a0e3df.js"
+  },
+  {
+    "revision": "6dac6d88bb7f236330eb8afa3cfce98a",
+    "url": "/static/media/Yvonne-nails.6dac6d88.jpg"
+  },
+  {
+    "revision": "a4d255b68821110386ea055fd4fb92da",
+    "url": "/static/media/allbeauty.a4d255b6.jpg"
+  },
+  {
+    "revision": "a9a7ec7beea26ea82d3ec656b19e2fb2",
+    "url": "/static/media/bg_3.a9a7ec7b.jpg"
+  },
+  {
+    "revision": "0f93b1dbc3749bf18bc3980286917a56",
+    "url": "/static/media/french.0f93b1db.jpg"
+  },
+  {
+    "revision": "83825973b1ff13790a8273e77f2c8c57",
+    "url": "/static/media/intu.83825973.jpg"
+  },
+  {
+    "revision": "04aecb96ecbf0ebab9e9b236dce5a1e0",
+    "url": "/static/media/me-shaping.04aecb96.jpg"
+  },
+  {
+    "revision": "d9323cf19a3bb1194df745c886d19f9b",
+    "url": "/static/media/mepainting.d9323cf1.jpg"
+  },
+  {
+    "revision": "3076132ecd0c374a3a58eea5a1798dd4",
+    "url": "/static/media/nails-marble.3076132e.jpg"
+  },
+  {
+    "revision": "2a44f7e38c2f104abe96d3b60de6e8bb",
+    "url": "/static/media/nails1.2a44f7e3.jpg"
+  },
+  {
+    "revision": "3a5a39b870aaa47b6db3773b9d3034f0",
+    "url": "/static/media/nails2.3a5a39b8.jpg"
+  },
+  {
+    "revision": "ada3c383a8830d425ec90e8533188a46",
+    "url": "/static/media/nails3.ada3c383.jpg"
+  },
+  {
+    "revision": "027bdbf7aab470aa0d27041530f5d3c7",
+    "url": "/static/media/occhi.027bdbf7.jpg"
+  },
+  {
+    "revision": "3df8fb0e08126bb486a8d4db1f7335ec",
+    "url": "/static/media/shellac.3df8fb0e.jpg"
+  },
+  {
+    "revision": "209d6f96c828f654d5c80ed221c13b05",
+    "url": "/static/media/sirsasana.209d6f96.jpeg"
+  },
+  {
+    "revision": "c1d89446d3a38aa536230d2bc344143d",
+    "url": "/static/media/thailand.c1d89446.jpg"
+  },
+  {
+    "revision": "3e0d90d94e1683dc101b7f4c207f0ada",
+    "url": "/static/media/yoga.3e0d90d9.jpg"
+  },
+  {
+    "revision": "e0ba39ce59736a0014ffcdf4a8f4ca06",
+    "url": "/static/media/yogaxx.e0ba39ce.jpg"
+  }
+]);
