faf3a7a7bd1b23b2733efb35833f4b831064843f
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2020-06-25 15:44:08 +0100

    changes

--- src/ValidationForm/Popup.js
+++
@@ -1,9 +0,0 @@
-import Popup from "reactjs-popup";
-import React from 'react';
-const popup = () => (
-  <Popup trigger={<button>Submit Booking</button>} position="right center">
-    <div>Thanks for your booking request, I will contact you as soon as possible time to confirm!</div>
-  </Popup>
-);
-
-export default popup;
