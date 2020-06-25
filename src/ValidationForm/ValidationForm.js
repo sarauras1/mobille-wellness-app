1fcb694a000a2ba5152a15525fcbb2fbdf601158
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2020-06-25 15:28:48 +0100

    changes

--- src/ValidationForm/ValidationForm.js
+++ src/ValidationForm/ValidationForm.js
@@ -1,298 +1,161 @@
 import React from "react";
-import ReactFormValidation from "react-form-input-validation";
-import './ValidationForm.css';
-
+import "./ValidationForm.css";
 
 const encode = (data) => {
   return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-}
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
-class ValidationForm extends React.Component {
+class Contact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      fields: {
-        customer_name: "",
-        customer_Surname: "",
-        email_address: "",
-        phone_number: "",
-        house_address: "",
-        pickupTratment: "",
-        comments: "",
-        AddOns: []
-      },
-      errors: {}
+      name: "",
+      surname: "",
+      email: "",
+      phone: "",
+      address: "",
+      message: "",
     };
-    this.form = new ReactFormValidation(this, { locale: "en" });
-    this.form.useRules({
-      customer_name: "required|username_available",
-      email_address: "required|email",
-      house_address: "required|address",
-      phone_number: "required|numeric|digits_between:10,12",
-      pickup_time: "required|date",
-      pickup_treatment: "requred|treatmet",
-       comments: "required|max:200",
-     
-    });
-
-       onsubmit = ( event) => {
-     
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "contact", ...this.state })
-      })
-        .then(() => alert("Thank you for your booking! We will contact you as soon has possible time for confirmation."))
-        .catch(error => alert(error));
-
-      event.preventDefault();
-    }
-    
-
-   
-    ReactFormValidation.registerAsync('username_available', function(username, attribute, req, passes) {
-      setTimeout(() => {
-        if (username === "foo")
-          passes(false, 'Username has already been taken.'); // if username is not available
-        else
-          passes();
-      }, 1000);
-    });
-    let messages = ReactFormValidation.getMessages('en');
-    messages.required = 'Whoops, :attribute field is required.';
-    ReactFormValidation.setMessages('en', messages);
-    ReactFormValidation.useLang('en')
   }
-    // when submit btn is clicked
-   
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = (e) => {
+    fetch("/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: encode({
+        "form-name": "contact",
+        ...this.state,
+      }),
+    })
+      .then(() => alert("Success! Thank you for your booking! We will contact you as soon has possible time for confirmation"))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = (e) =>
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+
   render() {
-    
+    const { name, surname, email, phone, address, date, message } = this.state;
     return (
-     
-        <div className="container">
+      <div className="container">
         <form
-          data-netlify-honeypot = "bot-field"
-            method="POST"
-            data-netlify="true"
-            name="contact"
-            action="/Thank you for your booking/contact"
-            className="myForm"
-            autoComplete="off"
-            onSubmit={this.handleSubmit}
-          >
-            <p>
-            <h3 className="text-centre-form">Book your treatment!</h3>
-              <label>
-                Name
-                <input
-                   type = "hidden"
-                   name = "form-name"
-                   value = "contact"
-                  onBlur={this.form.handleBlurEvent}
-                  onChange={this.form.handleChangeEvent}
-                  data-attribute-name="Customer Name"
-                  data-async
-                />
-              </label>
-              <label className="error">
-                {this.state.errors.customer_name
-                  ? this.state.errors.customer_name
-                  : ""}
-              </label>
-            </p>
-            <p>
-              <label>
-                Surname
-                <input
-                  type = "hidden"
-                  name = "form-name"
-                  value = "contact"
-                  onBlur={this.form.handleBlurEvent}
-                onChange={this.form.handleChangeEvent}
-                  // To override the attribute name
-                  data-attribute-name="Customer Surname"
-                  data-async
-                />
-              </label>
-              <label className="error">
-                {this.state.errors.customer_Surname
-                  ? this.state.errors.customer_Surname
-                  : ""}
-              </label>
-            </p>
-
-            <p>
-              <label>
-                Phone
-                <input
-                   type = "hidden"
-                   name = "form-name"
-                   value = "contact"
-                  onBlur={this.form.handleBlurEvent}
-                  onChange={this.form.handleChangeEvent}
-                 
-                />
-              </label>
-              <label className="error">
-                {this.state.errors.phone_number
-                  ? this.state.errors.phone_number
-                  : ""}
-              </label>
-            </p>
-
-            <p>
-              <label>
-                Email
-                <input
-                type="hidden"
-                name="form-name"
-                value="contact"
-                onBlur={this.form.handleBlurEvent}
-                onChange={this.form.handleChangeEvent}
-                />
-              </label>
-              <label className="error">
-                {this.state.errors.email_address
-                  ? this.state.errors.email_address
-                  : ""}
-              </label>
-            </p>
-            <p>
+          hidden
+          className="myForm"
+          onSubmit={this.handleSubmit}
+          data-netlify-honeypot="bot-field"
+          method="POST"
+          data-netlify="true"
+          name="contact"
+          action="/Thank you for your booking/contact"
+        >
+          <h3>Book your treatments</h3>
+          <p>
             <label>
-                Address
-                <input
-                 type = "hidden"
-                 name = "form-name"
-                 value = "contact"
-                  onBlur={this.form.handleBlurEvent}              
-                  onChange={this.form.handleChangeEvent}
-                />
-              </label>
-              <label className="error">
-                {this.state.errors.house_address
-                  ? this.state.errors.house_address
-                  : ""}
-              </label>
-            </p>
+              Your Name:{""}
+              <input
+                name="name"
+                value={name}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+          </p>
 
-           
-            <p>
-              <label>
-                Pickup treatments
-              
-                <select
-                  id="pickup_treatment"
-                  name="pickup_tratment"
-                  value={this.state.fields.pickup_treatment}
-                  onChange={this.form.handleChangeEvent}
-                  onBlur={this.form.handleBlurEvent}
-                >
-                  <option value="contact">Waxing</option>
-                  <option value="contact">Facial</option>
-                  <option value="contact">Massage</option>
-                  <option value="contact">Yoga</option>
-                  <option value="contact">Manicure</option>
-                  <option value="contact">Pedicure</option>
-                  <option value="contact">Shellac Manicure</option>
-                  <option value="contact">Shellac Pedicure</option>
-                </select>
-              </label>
-              <label className="error">
-                {this.state.errors.pickup_place
-                  ? this.state.errors.pickup_place
-                  : ""}
-              </label>
-            </p>
-            <fieldset>
-              <legend>Add ons treatments</legend>
-              <p>
-                <label className="choice">
-                  {" "}
-                  <input
-                    type="hidden" 
-                    name="form-name"
-                    onChange={this.form.handleChangeEvent}
-                    value="contact"
-                  />{" "}
-                  Shellac removal{" "}
-                </label>
-              </p>
-              <p>
-                <label className="choice">
-                  {" "}
-                  <input
-                   type="hidden" name="form-name" value="contact"
-                    onChange={this.form.handleChangeEvent}
-                  />{" "}
-                  Nails Repeir{" "}
-                </label>
-              </p>
-              <p>
-                <label className="choice">
-                  {" "}
-                  <input
-                   type="hidden" name="form-name" value="contact"
-                    onChange={this.form.handleChangeEvent}
-                    
-                  />{" "}
-                  Upper lips wax{" "}
-                </label>
-              </p>
-              <label className="option">
-                {this.state.errors.addOns
-                  ? this.state.option.addOns
-                  : ""}
-              </label>
-            </fieldset>
+          <p>
+            <label>
+              Your Surname:{" "}
+              <input
+                name="surname"
+                value={surname}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+          </p>
 
-            <p>
-              <label>
-                Pickup Date
-                <input
-                  type="hidden" name="form-name" value="contact"
-                  onChange={this.form.handleChangeEvent}
-                  onBlur={this.form.handleBlurEvent}
-                  value={this.state.fields.pickup_time}
-                />
-               
-              </label>
-              <label className="error">
-                {this.state.errors.pickup_time
-                  ? this.state.errors.pickup_time
-                  : ""}
-              </label>
-            </p>
+          <p>
+            <label>
+              Your Email:{" "}
+              <input
+                required
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+              />
+            </label>
+          </p>
 
-            <p>
-              <label>
-                Specify treatment
-                <textarea
-                  type="hidden"
-                   name="form-name"
-                    value="contact"
-                  maxLength="200"
-                  onChange={this.form.handleChangeEvent}
-                  onBlur={this.form.handleBlurEvent}
-                ></textarea>
-              </label>
-              <label className="error">
-                {this.state.errors.comments
-                  ? this.state.errors.comments
-                  : ""}
-              </label>
-            </p>
+          <p>
+            <label>
+              Your phone number:{" "}
+              <input
+                name="phone"
+                required
+                value={phone}
+                onChange={this.handleChange}
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              Address:{" "}
+              <input
+                name="address"
+                required
+                value={address}
+                onChange={this.handleChange}
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              Select Date:
+              <input
+                required
+                type="date"
+                onChange={this.handleChange}
+                value={date}
+              />
+            </label>
+          </p>
+          <p>
+            <label for="appt">
+              Select time:
+              <input type="time" name="appt"></input>
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:{" "}
+              <textarea
+                className="input-textarea"
+                required
+                name="message"
+                value={message}
+                onChange={this.handleChange}
+              />
+            </label>
+          </p>
 
-            <p>
-            <button className="form-button" type="submit">Submit Booking</button>
-            </p>
-          </form>
-        </div>
+          <p>
+            <button className="form-button" type="submit">
+              Submit booking
+            </button>
+          </p>
+        </form>
+      </div>
     );
   }
 }
 
-export default ValidationForm;
-
-
+export default Contact;
