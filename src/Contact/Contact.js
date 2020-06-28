581b26c2532fd4ea02db89c0aebffaaf277f9872
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2020-06-28 10:57:19 +0100

    changes

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,16 +1,43 @@
 
 import './Contact.css'
 import React from 'react'
+import React, { useState } from "react";
 
+const NameForm = (props) => {
+  const [name, setName] = useState("");
 
-
+  // This function puts all the form fields into a FormData constructor, which we later encode with the URLSearchParams constructor
+  const createFormDataObj = (data) => {
+    const formData = new FormData();
+    Object.keys(data).forEach((k)=>{
+      formData.append(k,data[k])
+    });
+    return formData
+  }
+  
+  const handleSubmit = (e) => {
+    // This `data` object is what's passed to the createFormDataObj. It needs all of your form fields, where the key is the name= attribute and the value is the value=
+    const data = { 
+      "form-name": "contact",
+      "name": name
+    }
+    // This POSTs your encoded form to Netlify with the required headers (for text; headers will be different for POSTing a file) and, on success, redirects to the custom success page using Gatsby's `navigate` helper function that we imported at the top
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(createFormDataObj(data)).toString()
+    })
+      .then(() => navigate("/Thank-you/"))
+      .catch(error => alert(error));
+    // This is required to prevent the default onSubmit behavior
+    e.preventDefault();
+  };
 
 const contact = () => {
      return (
        <div id="contact" className="container">
          <form
-          action="/Thank you for your booking will will contact 
-           you as soon as possible time to confirm"
+          action="/"
            className="myForm"
            name="contact"
            method="post"
