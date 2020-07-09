5096dd2f4e9b803298f79a19c664242617df055c
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2020-07-09 16:03:58 +0100

    changes

--- src/Massages/Deeptissue.js
+++ src/Massages/Deeptissue.js
@@ -20,7 +20,7 @@ const  Deeptissue = () => {
        margin: 'auto',
        padding: '10px',
        boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'}}>
-     1 hour deep tissue massage is a type of massage therapy that focuses on realigning deeper layers of muscles and 
+     Deep tissue massage is a type of massage therapy that focuses on realigning deeper layers of muscles and 
      connective tissue. It is especially helpful for chronic aches and pains and contracted areas such as stiff neck 
      and upper back, low back pain, leg muscle tightness, and sore shoulders. Some of the same strokes are used as 
      classic massage therapy, but the movement is slower and the pressure is deeper and concentrated on areas of tension 
--- src/Massages/Relaxing.js
+++ src/Massages/Relaxing.js
@@ -20,7 +20,7 @@ const  Relaxing = () => {
        margin: 'auto',
        padding: '10px',
        boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'}}>
-     1 hour full-body treatment involving soft music, soft lighting and aromatherapy oils. You will have a long, luxurious 
+     Full-body treatment involving soft music, soft lighting and aromatherapy oils. You will have a long, luxurious 
      massage which will use all kinds of techniques including long slow movements, gentle kneading and rolling of skin and muscle, and perhaps some rhythmic rocking from side to side. It is a passive treatment. You probably won't be stretched or 
      manoeuvred into any yoga-like positions, as you are, for example, in Thai massage. You just lie there and take it.
       <br/>
