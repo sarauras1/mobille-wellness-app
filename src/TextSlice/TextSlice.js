c74b934498c00ed100dd78c74459dc0ed40c07ec
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2020-09-21 19:11:59 +0100

    changes

--- src/TextSlice/TextSlice.js
+++ src/TextSlice/TextSlice.js
@@ -9,9 +9,9 @@ const textSlice = props => {
   <div  className="top-right">
 <motion.div
   
-   transition={{ duration: 10 }}  
+   transition={{ duration: 5 }}  
   initial={{ scale: 0 }}
- animate={{ scale: 1, rotate: 10}}
+ animate={{ scale: 1}}
       
 >
 <h1 id="top-right-h1">MOBILE WELLNESS</h1>
