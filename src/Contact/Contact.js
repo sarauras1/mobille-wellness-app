fefddb0e81fbc29625e25d90a6037a5133d5e4da
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2020-11-11 12:45:19 +0000

    changes

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -71,19 +71,18 @@ class Contact extends React.Component {
               Thanks for choosing All Beauty By Sara London Mobile Beauty
               Services. Book your treatments using the form below. If you have
               inqueries about my services do not hesitate to contact me via
-              WhatsApp or email.
-               Please note that All Beauty By Sara does not
-              accept bookings under £30 or £50 or more... depending on the area you live and
-              the time you book.
-               There may be a futher surcharge if you live in the congestion charge 
-               zone or if you don't have free parking readily available.
-               
+              WhatsApp or email. Please note that All Beauty By Sara does not
+              accept bookings under £30 or £50 or more... depending on the area
+              you live and the time you book. There may be a futher surcharge if
+              you live in the congestion charge zone or if you don't have free
+              parking readily available.
             </p>
             <p>
               <label>
                 Your Name:{" "}
                 <input
-                  id=""
+                  id="name"
+                  placeholder="Your Name"
                   type="text"
                   name="name"
                   className="input"
@@ -97,6 +96,7 @@ class Contact extends React.Component {
               <label>
                 Your Surname:{" "}
                 <input
+                  placeholder="Your Surname"
                   id="surname"
                   type="text"
                   name="surname"
@@ -111,6 +111,7 @@ class Contact extends React.Component {
               <label>
                 Your Email:{" "}
                 <input
+                  placeholder="Your Email"
                   type="email"
                   required
                   id="email"
@@ -125,6 +126,7 @@ class Contact extends React.Component {
               <label>
                 Your phone number:{" "}
                 <input
+                  placeholder="Your Phone Number"
                   type="tel"
                   name="tel"
                   id="tel"
@@ -139,6 +141,7 @@ class Contact extends React.Component {
               <label>
                 Address:{" "}
                 <input
+                  placeholder="Your Address"
                   type="address"
                   name="address"
                   id="address"
@@ -154,6 +157,7 @@ class Contact extends React.Component {
                 Select Date:{" "}
                 <input
                   required
+                  placeholder="Select Date"
                   name="date"
                   type="date"
                   id="date"
@@ -167,6 +171,7 @@ class Contact extends React.Component {
               <label>
                 Select time:{" "}
                 <input
+                  placeholder="Select Time"
                   type="time"
                   name="appt"
                   id="appt"
@@ -181,8 +186,9 @@ class Contact extends React.Component {
             </p>
             <p>
               <label>
-                treatments to book:{" "}
+                treatments:{" "}
                 <textarea
+                  placeholder="Specify the treatments you want book"
                   className="input-textarea"
                   required
                   id="message"
