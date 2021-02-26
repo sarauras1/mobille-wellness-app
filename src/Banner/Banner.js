404a7cd3d2d8e2ab34dbac665a139fb3b2d3977e
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2021-02-26 19:54:09 +0000

    changes

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './Banner.css';
 import banner2 from './banner2.gif';
-import phone from './bannersmall.png';
-
 
 const Banner = () => {
   return (
     <div id="banner" className="Banner-Slicer">
       <div className="Banner-Slicer">
         <img className="banner2" src={banner2} alt="wellness" />
-        <img className="bannerphone" src={phone} alt="beauty" />
+       <h1 className="Banner-text">Welcome to All Beauty By Sara</h1>
       </div>
     </div>
   );
