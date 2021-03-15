26dc0de8afbd437e5044bd2d155fd6155353aa2d
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2021-03-15 17:17:30 +0000

    changes

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -6,24 +6,35 @@ const Banner = () => {
   return (
     <div id="banner" className="Banner-Slicer">
       <div className="Banner-Slicer">
-        <img className="banner2" src={banner2} alt="wellness" />
-        <img className="banner1" src={banner1} alt="wellness" />
+        <img className="banner2" src={banner2} alt="wellness" loading="lazy" />
+        <img className="banner1" src={banner1} alt="wellness" loading="lazy" />
         <div className="div-banner-small">
           <h1
             data-aos="fade-right"
+            data-aos-offset="20"
+            data-aos-delay="50"
+            data-aos-duration="2000"
             data-aos-easing="ease"
+            data-aos-mirror="true"
+            data-aos-once="false"
             data-aos-anchor-placement="top-center"
-            className="banner-title"
           >
-            The Mobile Wellness
+            data-aos-easing="ease" data-aos-anchor-placement="top-center"
+            className="banner-title" > The Mobile Wellness
           </h1>
           <p
             className="banner-para"
             data-aos="fade-left"
             data-aos-easing="ease"
+            data-aos-offset="20"
+            data-aos-delay="50"
+            data-aos-duration="2000"
+            data-aos-mirror="true"
+            data-aos-once="false"
             data-aos-anchor-placement="top-center"
           >
-            Book your treatments at the comfort of your own home
+            data-aos-anchor-placement="top-center" > Book your treatments at the
+            comfort of your own home
           </p>
         </div>
       </div>
