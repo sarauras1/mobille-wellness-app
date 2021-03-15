3096fdf436f39ac7eee5906f241968b69e1a457f
Sara <41049600+sarauras1@users.noreply.github.com>
2021-03-15 12:49:12 +0000

    Update Banner.js

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import './Banner.css';
 import banner2 from './banner2.gif';
@@ -8,6 +10,26 @@ const Banner = () => {
       <div className="Banner-Slicer">
         <img className="banner2" src={banner2} alt="wellness" />
         <img className="banner1" src={banner1} alt="wellness" />
+        <div className="div-banner-small">
+          <h1
+            data-aos="fade-right"
+      
+            data-aos-easing="ease"
+            data-aos-anchor-placement="top-center"
+            className="banner-title"
+          >
+            The Mobile Wellness
+          </h1>
+          <p
+            className="banner-para"
+            data-aos="fade-left"
+           
+            data-aos-easing="ease"
+            data-aos-anchor-placement="top-center"
+          >
+            Book your treatments at the comfort of your own home
+          </p>
+        </div>
       </div>
     </div>
   );
@@ -15,5 +37,9 @@ const Banner = () => {
 
 
   export default Banner;
+
+
+
+ 
 
 
