12f22ab15181b542af1c332c594a369d099446d6
sarauras1 <41049600+sarauras1@users.noreply.github.com>
2021-03-29 10:58:13 +0100

    changes

--- src/Reviews.js
+++ src/Reviews.js
@@ -1,55 +1,55 @@
-import React, { Component } from "react";
-import axios from "axios";
-import "./App.css";
+// import React, { Component } from "react";
+// import axios from "axios";
+// import "./App.css";
 
 
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      review: "",
-      stars: "",
-    };
-  }
-  makePostRequest() {
-    return axios({
-      method: "POST",
-      url:
-        "https://mybusiness.googleapis.com/v3/accounts/{process.env.ACCOUNT_ID}/locations/{process.env.LOCATION_ID}/reviews/",
-      data: {
-        name: "",
-        review: "",
-        stars: "",
-      },
-    }).then((response) => response.data.json);
-  }
-  async componentDidMount() {
-    try {
-      let postResult = await this.makePostRequest();
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: "",
+//       review: "",
+//       stars: "",
+//     };
+//   }
+//   makePostRequest() {
+//     return axios({
+//       method: "POST",
+//       url:
+//         "https://mybusiness.googleapis.com/v3/accounts/{process.env.ACCOUNT_ID}/locations/{process.env.LOCATION_ID}/reviews/",
+//       data: {
+//         name: "",
+//         review: "",
+//         stars: "",
+//       },
+//     }).then((response) => response.data.json);
+//   }
+//   async componentDidMount() {
+//     try {
+//       let postResult = await this.makePostRequest();
     
-      this.setState({
-        name: postResult.name,
-        review: postResult.review,
-        stars: postResult.stars,
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  }
-  render() {
-    const { name, review, stars } = this.state;
-    return (
-      <div className="App">
-        <h2>{name}</h2>
-        <h2>{review}</h2>
-        <h2>{stars}</h2>
+//       this.setState({
+//         name: postResult.name,
+//         review: postResult.review,
+//         stars: postResult.stars,
+//       });
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   }
+//   render() {
+//     const { name, review, stars } = this.state;
+//     return (
+//       <div className="App">
+//         <h2>{name}</h2>
+//         <h2>{review}</h2>
+//         <h2>{stars}</h2>
         
-      </div>
-    );
-  }
-}
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
